72cfaab8bb80b1e6f2b3e7601998fa906ae8edda
Jakub Bojarski <jakub78b@gmail.com>
2026-09-10 10:21:31 +0100

    Added UserDetailsImpl

---
+++ src/main/java/com/example/imageprocessingservice/security/UserDetailsImpl.java
@@ -0,0 +1,71 @@
+package com.example.imageprocessingservice.security;
+
+import com.example.imageprocessingservice.domain.UserRecord;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Data
+public class UserDetailsImpl implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String username;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean enabled;
+
+    public static UserDetailsImpl build(UserRecord userRecord) {
+        List<GrantedAuthority> authorities = userRecord.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImpl(
+                userRecord.getId(),
+                userRecord.getUsername(),
+                userRecord.getEmail(),
+                userRecord.getPassword(),
+                authorities,
+                userRecord.isEnabled()
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
